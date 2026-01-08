cfd32cf1d6cfd33fd14c96e9f023523b3d95a57a
Bảo Dư <duybao1403@gmail.com>
2026-01-08 22:45:28 +0700

    Two numbers sum using two nested for-loop

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = twoNumberSum(new int[]{2, 7, 11, 15}, 9);
+        if (array.length == 0) {
+            System.out.println("No two numbers found");
+        }else{
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+    /*Write a func that takes in a non-empty array of distinct integers and an integer representing
+    a target sum. If any two numbers in the input array sum up to the target sum, the function should
+    return them in an array, in any order. If no two numbers sum up to the target sum, the func
+    should return an empty array.
+    Note that the target sum has to be obtained by summing two different integers in the array;
+    you can't add a single integer to itself in order to obtain the target sum.
+    You can assume that there will be at most one pair of numbers summing up to the target sum.*/
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == targetSum) {
+                    return new int[]{array[i], array[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
