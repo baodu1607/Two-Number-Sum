ad2498c6cdedb117d7120a24af1407a354f346b5
Bảo Dư <duybao1403@gmail.com>
2026-01-09 11:34:24 +0700

    Adding a two-pointer approach with O(n log n) time complexity and O(1) space

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = twoNumberSum2(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10);
+        int[] array = twoNumberSum3(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 13);
         if (array.length == 0) {
             System.out.println("No two numbers found.");
         }else{
@@ -19,7 +20,7 @@ public class Main {
     you can't add a single integer to itself in order to obtain the target sum.
     You can assume that there will be at most one pair of numbers summing up to the target sum.*/
 
-    //O(n^2) time / O(1) space
+    //O(n^2) time, O(1) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
         for (int i = 0; i < array.length-1; i++) {
             for (int j = i + 1; j < array.length; j++) {
@@ -31,7 +32,7 @@ public class Main {
         return new int[0];
     }
 
-    // O(n)
+    // O(n) time, O(n) space
     public static int[] twoNumberSum2(int[] array, int targetSum) {
         HashSet<Integer> traversedElem = new HashSet<>();
         for (int j : array) {
@@ -44,9 +45,22 @@ public class Main {
         return new int[0];
     }
 
-    //O(nlogn), O(1) space
+    //O(nlogn) time, O(1) space
+    //it changes the origin array
     public static int[] twoNumberSum3(int[] array, int targetSum) {
-
+        Arrays.sort(array); //dual-pivot quicksort
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int currentSum = array[left] + array[right];
+            if(currentSum == targetSum){
+                return new int[]{array[left], array[right]};
+            }else if(currentSum < targetSum){
+                left++;
+            }else{
+                right--;
+            }
+        }
         return new int[0];
     }
 }
